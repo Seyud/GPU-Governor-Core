2d60c8d62c0d5d8c9e57220bf540f1ecca20f4b2
Seyud <2159455958@qq.com>
2025-07-26 07:21:38 +0800

    chore: 更新开发者信息中的联系方式

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,7 +1,7 @@
 /// GPU Governor 常量定义
 /// 将分散的常量集中管理，提高代码可维护性
 pub const NOTES: &str = "Mediatek Mali GPU Governor";
-pub const AUTHOR: &str = "Author: walika @CoolApk, rtools @CoolApk";
+pub const AUTHOR: &str = "Author: walika @CoolApk, localhost_hutao @Telegram";
 pub const SPECIAL: &str =
     "Special Thanks: HamJin @CoolApk, asto18089 @CoolApk and helloklf @Github";
 pub const VERSION: &str = "Version: v2.8.0";
