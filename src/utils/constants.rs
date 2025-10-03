86da8a5c2580d91f64373916bf4a435060a6be70
Seyud <2159455958@qq.com>
2025-10-04 00:22:00 +0800

    docs(utils/constants): 更新作者信息和版本号
    更新作者信息中的GitHub用户名，并将版本号从v2.9.0升级到v2.10.0

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,10 +1,10 @@
 /// GPU Governor 常量定义
 /// 将分散的常量集中管理，提高代码可维护性
 pub const NOTES: &str = "Mediatek Mali GPU Governor";
-pub const AUTHOR: &str = "Author: walika @CoolApk, localhost_hutao @Telegram";
+pub const AUTHOR: &str = "Author: walika @CoolApk, Tools-cx-app @GitHub";
 pub const SPECIAL: &str =
-    "Special Thanks: HamJin @CoolApk, asto18089 @CoolApk and helloklf @Github";
-pub const VERSION: &str = "Version: v2.9.0";
+    "Special Thanks: HamJin @CoolApk, asto18089 @CoolApk and helloklf @GitHub";
+pub const VERSION: &str = "Version: v2.10.0";
 
 /// GPU 调频策略常量
 pub mod strategy {
